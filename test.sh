5088fc4d4de6e8e85d2a62e2ed04ef030fd99d6a
zrthstr <zrth1k@gmail.com>
2025-01-02 12:05:18 +0100

    ..

--- test.sh
+++ test.sh
@@ -6,5 +6,10 @@ echo "this is still the orignal"
 env | sed 's/./& /g'
 
 echo "--------------------------------"
+ls /home/runner/work/_temp
+find /home/runner/work/_temp
+echo "--------------------------------"
+
+echo "--------------------------------"
 echo "test stop"
 echo "--------------------------------"
