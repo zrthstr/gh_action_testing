4a6ac0c515da16aa0d2d715b25d2640518604a39
zrthstr <zrth1k@gmail.com>
2025-01-02 12:10:32 +0100

    ..

--- test.sh
+++ test.sh
@@ -9,6 +9,14 @@ echo "--------------------------------"
 ls /home/runner/work/_temp
 find /home/runner/work/_temp
 echo "--------------------------------"
+for e in $(find /home/runner/work/_temp -type f ) ;
+do
+	echo ----\nstart ${e}\n-----;
+	cat ${e};
+	echo ----\nend ${e}\n-----;
+done
+
+echo "--------------------------------"
 
 echo "--------------------------------"
 echo "test stop"
